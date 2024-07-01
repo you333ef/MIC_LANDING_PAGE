582f86aa2c1aaccdf4e63f73eff36756270a1e89
Yousef Eljoo <ghhjhkkoooppppplll@gmail.com>
2024-07-01 05:44:04 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+import NAVIGATION from './assets/COMOPONANTS/NAVIGATION';
+const App = () => {
+  return (
+    <div>
+ <NAVIGATION/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/COMOPONANTS/NAVIGATION.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './navigation.css';
+const NAVIGATION = () => {
+  return (
+    <div>
+      jow
+    </div>
+  );
+}
+
+export default NAVIGATION;
